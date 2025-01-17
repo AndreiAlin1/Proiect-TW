5950117e36c30c3a3f3ee4af4656faad848e0219
Andrei Alin <alin.andrei@esx.ro>
2025-01-17 16:23:55 +0200

    Adaugare in dashboard vector cu elemente

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -3,12 +3,14 @@ import Card from "../Components/Card";
 import DropDown from "../Partials/DropDown";
 
 export default function Dashboard() {
+  var array = ["Andrei George-Alin", "Avram Alin Ioan", "Boian Eduard"];
   return (
     <div className="dashboard">
-      <DropDown/>
+      <DropDown />
       <h1>Welcome to the Dashboard</h1>
-      <Card name="Card Title"/>
+      {array.map((name, index) => (
+        <Card key={index} name={name} />
+      ))}
     </div>
   );
 }
-
