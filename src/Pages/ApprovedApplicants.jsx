c5c929ce1bf0bfea5d450a2b03b9ec5a601379bf
Andrei Alin <alin.andrei@esx.ro>
2025-01-17 16:06:38 +0200

    Adaugare pagina vizualizare studenti acceptati

---
+++ src/Pages/ApprovedApplicants.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DropDown from "../Partials/DropDown";
+import CardStudent from "../Components/CardStudent";
+
+//trb facut sa ia din bd fiecare aplicant specific unui id al unui profesor
+export default function ApprovedApplicants() {
+    return (
+        <div>
+            <h1>Approved Applicants</h1>
+            <DropDown />
+            <CardStudent name="Andrei George-Alin" specializare="Informatica"  serie = "C" grupa = "1087"/>
+        </div>
+    );
+}
